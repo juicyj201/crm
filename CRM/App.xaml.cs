219429e3c71ad202f83c6b50713c2232db560c86
Joshua Julies <joshuajulies06@gmail.com>
2024-07-09 19:27:08 +0200

    Added projects

---
+++ CRM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CRM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ CRM/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
